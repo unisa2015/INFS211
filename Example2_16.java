bd4fd7e72e35140961e4c941c4d38eecac97c9a7
unisa2015 <kgotlagopane@gmail.com>
2023-04-19 20:45:11 +0200

    Add files via upload

---
+++ Example2_16.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Example2_16
+{
+static Scanner console = new Scanner(System.in);
+public static void main(String[] args)
+{
+int feet;
+int inches;
+System.out.println("Enter two integers separated by spaces.");
+feet = console.nextInt();
+inches = console.nextInt();
+System.out.println("feet = " + feet);
+System.out.println("inches = " + inches);
+}
+}
